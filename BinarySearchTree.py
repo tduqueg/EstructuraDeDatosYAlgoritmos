debd6f729a6ad75f4d341e75ebbd1f9fcb1eacfc
Tomás Duque <tduqueg@eafit.edu.co>
2023-03-15 21:37:03 -0500

    Arbol Binario Funcionando

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -42,34 +42,61 @@ class BinarySearchTree:
     
     def get_min(self):
         
-        if self.node:
+        if self.root:
             return self.get_min_value(self.root)
     
     def get_min_value(self,node):
         
         if node.left_node:
-            self.get_min_value(node.left_node)
+            return self.get_min_value(node.left_node)
         
-        return node.value
+        return node.data
 
     def get_max(self):
-        if self.node:
+        if self.root:
             return self.get_max_value(self.root)
     
     def get_max_value(self,node):
 
         if node.right_node:
-            self.get_max_value(node.right_node)
+            return self.get_max_value(node.right_node)
 
-        return node.value
+        return node.data
     
     def traverse(self):
        
         if self.root:
             self.traverse_in_order(self.root)
 
+    #Tiene una complejidad de O(n) debido a que debemos visitar cada uno de los ítems
     def traverse_in_order(self,node):
         
+        if node.left_node:
+            self.traverse_in_order(node.left_node)
+
+        print(node.data)
+
+        if node.right_node:
+            self.traverse_in_order(node.right_node)
+
+if __name__ == '__main__':
+    bst = BinarySearchTree()
+    bst.insert(10)
+    bst.insert(9)
+    bst.insert(20)
+    bst.insert(32)
+    bst.insert(-2)
+    bst.insert(3)
+    bst.insert(200)
+    bst.insert(-50)
+    bst.insert(69)
+    bst.insert(-96)
+    bst.traverse()
+    print("Valor Máximo: %d" % bst.get_max())
+    print("Valor Minimo: %d" % bst.get_min())
+    
+        
+
 
 
         
